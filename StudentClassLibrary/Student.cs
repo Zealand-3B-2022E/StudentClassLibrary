cabadf089aba83696a5b273ca36bc7690a2206b2
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2022-09-26 13:52:52 +0200

    Add project files.

---
+++ StudentClassLibrary/Student.cs
@@ -0,0 +1,28 @@
+﻿namespace StudentClassLibrary
+{
+    /// <summary>
+    /// Student class indeholder data om de studerende
+    /// navn mv.
+    /// </summary>
+    public class Student
+    {
+        public string MyName { get; set; }
+
+
+        /// <summary>
+        /// Denne metode returnere navnet på den studerende
+        /// </summary>
+        /// <returns> "Mit navn: " mynavn </returns>
+        public string MyNameText()
+        {
+            return "Mit navn: " + MyName;
+        }
+
+        public string MYNEWNameText()
+        {
+            return "Noegt nyt tejkst";
+        }
+
+
+    }
+}
